3f83e43c5e05952f53ed29e7426caca8be134a26
kulikpasha <110726627+kulikpasha@users.noreply.github.com>
2024-05-25 03:08:50 +0300

    finished backend, add reg/login/auth

--- server/routes/recipesRouter.js
+++ server/routes/recipesRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const recipesController = require('../controllers/recipesController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', recipesController.create)
+router.post('/', checkRole('ADMIN'),recipesController.create)
 router.get('/', recipesController.getAll)
 router.get('/:id', recipesController.getOne)
 
--- server/routes/typesRouter.js
+++ server/routes/typesRouter.js
@@ -1,8 +1,9 @@
 const Router = require('express')
 const router = new Router()
 const typesController = require('../controllers/typesController')
+const checkRole = require('../middleware/checkRoleMiddleware')
 
-router.post('/', typesController.create)
+router.post('/', checkRole('ADMIN'), typesController.create)
 router.get('/', typesController.getAll)
 
 
